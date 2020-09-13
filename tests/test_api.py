c6301c5da6203381f122b87ab27199bea9b1e6ce
Geulmaster <eyal.geulayev@gmail.com>
2020-09-13 23:13:46 +0300

    Add register test and make different dict for each test

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,11 +5,21 @@ basic_user_info = {"username": "Eyal", "password": "chicaloca"}
 
 class Tests(unittest.TestCase):
 
+    def test_register(self):
+        """
+        Register as a new client
+        """
+        register_credentials = basic_user_info.copy()
+        register_credentials["username"] = "Eyalp"
+        with app.test_client(self) as tester:
+            req = tester.post('/register', json = register_credentials)
+        self.assertEqual(req.get_json(), {'msg': 'Successfully signed up for the API', 'status': 200})
+
     def test_add(self):
         """
         Add 500 Jubot to Eyal
         """
-        add_credentials = basic_user_info
+        add_credentials = basic_user_info.copy()
         add_credentials["amount"] = 500
         with app.test_client(self) as tester:
             req = tester.post('/add', json = add_credentials)
@@ -19,9 +29,9 @@ class Tests(unittest.TestCase):
         """
         Test balance functionality
         """
-        add_credentials = basic_user_info
+        balance_credentials = basic_user_info.copy()
         with app.test_client(self) as tester:
-            req = tester.post('/balance', json = add_credentials)
+            req = tester.post('/balance', json = balance_credentials)
         self.assertEqual(req.status_code, 200)
         return req.get_json()
 
@@ -29,12 +39,12 @@ class Tests(unittest.TestCase):
         """
         Test transfer functionality
         """
-        add_credentials = basic_user_info
-        add_credentials["to"] = "BANK"
-        add_credentials["amount"] = 100
+        transfer_credentials = basic_user_info.copy()
+        transfer_credentials["to"] = "BANK"
+        transfer_credentials["amount"] = 100
         amount_before = self.test_balance()["Own"]
         with app.test_client(self) as tester:
-            req = tester.post('/transfer', json = add_credentials)
+            req = tester.post('/transfer', json = transfer_credentials)
         amount_after = self.test_balance()["Own"]
         self.assertEqual(amount_before - amount_after, 100)
         self.assertEqual(req.status_code, 200)
